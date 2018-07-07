6e98de69e376184a6bf6c47be1edf9f06abdeed8
Niels Smit <nertniels@gmail.com>
2018-07-07 16:43:53 +0200

    Finished LayerBase

--- FeedForward/Layers/LayerBase.cs
+++ FeedForward/Layers/LayerBase.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using FeedForward.Core;
+
 namespace FeedForward.Layers
 {
     abstract class LayerBase
     {
+
+        public int nodes;
+
+        public Matrix values;
+        public Matrix weights;
+        public Matrix errors;
+        public Matrix bias;
+
+        public abstract void FeedForward(LayerBase input);
+        public abstract void Backpropagate(LayerBase output);
+
+        public abstract void initWeights();
 
     }
 }
