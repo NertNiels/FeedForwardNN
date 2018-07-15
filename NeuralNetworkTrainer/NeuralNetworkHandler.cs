4f257a9712b1351158959be7674da8e1c0aa89bb
Niels Smit <nertniels@gmail.com>
2018-07-15 18:09:45 +0200

    Began w/ making the NN working on web

--- NeuralNetworkTrainer/NeuralNetworkHandler.cs
+++ NeuralNetworkTrainer/NeuralNetworkHandler.cs
@@ -16,5 +16,45 @@ namespace NeuralNetworkTrainer
         {
             network = new Model(layers);
         }
+        
+        public static LayerBase[] getLayers()
+        {
+            if (network == null) return null;
+            return network.layers;
+        } 
+
+        public static void Train()
+        {
+            for(int i = 0; i < DataKeeper.DataSet.Length; i++)
+            {
+
+            }
+        }
+    }
+
+    public static class DataKeeper
+    {
+        public static List<float> Loss = new List<float>();
+
+        public static Data[] DataSet;
+
+        public static void shuffleDataSet()
+        {
+            Random r = new Random();
+            
+            for(int i = 0; i < DataSet.Length; i++)
+            {
+                int b = r.Next(DataSet.Length - i);
+                Data t = DataSet[i];
+                DataSet[i] = DataSet[b];
+                DataSet[b] = t;
+            }
+        }
+    }
+
+    public class Data
+    {
+        public Matrix Inputs;
+        public Matrix Targets;
     }
 }
